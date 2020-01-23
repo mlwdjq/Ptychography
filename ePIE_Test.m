2ee18332d23074f91e91d39d4f75d65557d853e0
Wenhua <mlwdjq@lbl.gov>
2020-01-23 13:41:06 -0800

    init

---
+++ ePIE_Test.m
@@ -0,0 +1,7 @@
+%% this script is used for test the ePIE algorithm
+% reference: Maiden, A. M., & Rodenburg, J. M. (2009). 
+% An improved ptychographical phase retrieval algorithm for 
+% diffractive imaging.?Ultramicroscopy,?109(10), 1256-1262.
+
+%% parameters setting
+
